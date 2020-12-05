111f529cff48d058df0a473cf33e0e9f7f6ff35a
comeidone <giancarlodelfine@gmail.com>
2020-12-05 23:55:50 +0100

    test sugestion

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import requests
 def main ():
     req=requests.get("http://google.it")
     print("REQUEST IS: ",req.reason)
-
+    
+    #TODO: change file name with page @comeidone
     writer=open("google.txt","w")
     writer.write(req.text)
     writer.close
