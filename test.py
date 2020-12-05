b719a172271374808199a71bfb535bdeb6255803
comeidone <giancarlodelfine@gmail.com>
2020-12-06 00:18:23 +0100

    remove todo

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ def main ():
     req=requests.get("http://google.it")
     print("REQUEST IS: ",req.reason)
     
-    #TODO: #2 change file name with page @comeidone
     writer=open("page.txt","w")
     writer.write(req.text)
     writer.close
