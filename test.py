bbdac4f3f79768b0a44667d4feaf2a0de2b44f80
comeidone <giancarlodelfine@gmail.com>
2020-12-06 00:13:21 +0100

    resolved issue

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ def main ():
     req=requests.get("http://google.it")
     print("REQUEST IS: ",req.reason)
     
-    #TODO: change file name with page @comeidone
-    writer=open("google.txt","w")
+    #TODO: #2 change file name with page @comeidone
+    writer=open("page.txt","w")
     writer.write(req.text)
     writer.close
 
